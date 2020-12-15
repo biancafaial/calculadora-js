901dafd83abf3ccd88c81b267eebe8d9ea3e658b
biancafaial <biancafaial@gmail.com>
2020-12-15 16:19:11 -0300

    refatoração do objeto calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,38 +1,57 @@
 var Calculadora = {
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISA0: '/',
+    MULTIPLICACAO: '*',
+
     adicionar: function (num1, num2) {
 
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0;
-        }
-
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-
-        return num1 + num2;
+        return Calculadora.calcular(num1, num2, 
+            Calculadora.ADICAO);
     },
     subtrair: function (num1, num2) {
-        
+        return Calculadora.calcular(num1, num2, 
+            Calculadora.SUBTRACAO);
+    },
+    dividir: function (num1, num2) {
+        if (num2 == 0) {
+            return 'ERRO';
+        }
+        return Calculadora.calcular(num1, num2, 
+            Calculadora.DIVISA0);
+    },
+    multiplicar: function (num1, num2) {
+        return Calculadora.calcular(num1, num2, 
+            Calculadora.MULTIPLICACAO);
+    },
+    calcular: function (num1, num2, operacao) {
+        var result = 0; 
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
         if (isNaN(num1) || isNaN(num2)) {
             return 0;
         }
+        switch(operacao){
+            case Calculadora.ADICAO:
+                result = num1 + num2;
+                break;
+            case Calculadora.SUBTRACAO:
+                result = num1 - num2;
+                break;
+            case Calculadora.DIVISA0:
+                result = num1 / num2;
+                break;
+            case Calculadora.MULTIPLICACAO:
+                result = num1*num2;
+                break;
+            default:
+                result =0;
+                break
+        }
 
-        return num1 - num2;
-    },
-    dividir: function (num1, num2) {
-        if(num2 == 0){
-            return 'ERRO';
-        }
-        if(isNaN(num1) || isNaN(num2)){
-            return 0;
-        }
-        return num1 / num2;
-    },
-    multiplicar: function (num1, num2) {
-        if(isNaN(num1) || isNaN(num2)){
-            return 0;
-        }
-        return num1*num2;
-    },
+        return result;
+
+    }
 };
 // usado na função 'require' do NodeJS
 if (typeof module !== 'undefined'
